798335905a243633246e76ffddd769c263da12e8
Victor Baleroni <victorbaleroni007@gmail.com>
2023-11-21 10:41:25 -0300

    login atualizado

--- login.php
+++ login.php
@@ -1,1 +1,21 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>CRUD-Login</title>
+</head>
+<body>
+    <h1>Criar conta</h1>
+    <form method="POST" action="">
+    <input class="form-control" type="text" name="cad_usuario" placeholder="Usuario">
+        <br><br>
+    <input class="form-control" type="password" name="cad_senha" placeholder="senha">
+        <br><br>
+    <input type="submit" name="cad_login_button" value="cadastrar">
+    </form>
+    
+</body>
+</html>
+
