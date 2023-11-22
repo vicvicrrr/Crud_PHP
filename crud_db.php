bb4c6531b98b970eb7c6eb471d35e6e7e4efc994
Victor Baleroni <victorbaleroni007@gmail.com>
2023-11-22 16:35:15 -0300

    banco, tabela e botao excluir

--- crud_db.php
+++ crud_db.php
@@ -31,16 +31,20 @@ $dado_prod = filter_input_array(INPUT_POST, FILTER_DEFAULT);
 $produto = new \banco\Produto();
 $produtoDao = new \banco\ProdutoDao();
 
-//$produto->setId(1);
+
 
 //$produtoDao->update($produto);
-
-//$produtoDao->delete();
+if(!empty($_POST['id'])){
+$produtoDao->delete($_POST['id']);
+}
 
  foreach($produtoDao->read() as $produto){
      echo "<hr>".$produto['id']."<br>".$produto['nome']."<hr>";
-     echo "<input type='hiden' value='".htmlspecialchars($produto['id'])."'/><hr>";
+     //echo "<input type='hiden' value='".htmlspecialchars($produto['id'])."'/><hr>";
+
+     ?><form action="" method="POST">
+       <input type="hidden" name="id" value="<?php echo $produto['id'];?>">
+       <input type="submit" name="deletar" value="deletar">
+       </form><?php
  }
-
-
 ?>
